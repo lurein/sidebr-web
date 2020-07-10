7bf7002330fa658a3c3d23d2ce6fa842c07d4a87
Lurein Perera <pererag@bc.edu>
2020-07-10 16:56:13 +0200

    updated new download links

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -5,15 +5,15 @@
       <div class="columns is-centered is-vcentered">
         <div class="column is-full">
           <h2 class="is-size-1">Here We Are</h2>
-          <h2 class="is-size-2">You're One Click Away From Liftoff</h2>
+          <h2 class="is-size-2">You're One Click Away From Liftoff 🚀</h2>
           <a class="button is-medium"
            style="background-color: #6890F6; color: #FFF"
-           href="https://srv-file8.gofile.io/download/4YgEMu/sidebr-0.2.0.dmg">
+           href="https://sidebr.imfast.io/sidebr-0.2.1.dmg">
             Download for Mac
           </a>
           <a class="button is-medium"
            style="background-color: #6890F6; color: #FFF"
-           href="https://srv-file8.gofile.io/download/4YgEMu/sidebr%20Setup%200.2.0.exe">
+           href="https://sidebr.imfast.io/sidebr%20Setup%200.2.1.exe">
             Download for Windows
           </a>
         </div>
