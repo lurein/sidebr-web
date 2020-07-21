2c3800e5eff196309a2c805c8f940db1370dde6b
Lurein Perera <pererag@bc.edu>
2020-07-21 13:19:47 -0400

    updated downlaod links to v0.2.5

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -8,12 +8,12 @@
           <h2 class="is-size-2">You're One Click Away From Liftoff 🚀</h2>
           <a class="button is-medium"
            style="background-color: #6890F6; color: #FFF"
-           href="https://sidebr.imfast.io/sidebr-0.2.3.dmg">
+           href="https://sidebr.imfast.io/sidebr-0.2.5.dmg">
             Download for Mac
           </a>
           <a class="button is-medium"
            style="background-color: #6890F6; color: #FFF"
-           href="https://sidebr.imfast.io/sidebr%20Setup%200.2.3.exe">
+           href="https://sidebr.imfast.io/sidebr%20Setup%200.2.5.exe">
             Download for Windows
           </a>
         </div>
